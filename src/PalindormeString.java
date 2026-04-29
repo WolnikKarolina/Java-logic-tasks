0269a372e9d5562f3eef2ffb0617cb349179e88a
Karola <wolnikkarolina1@gmail.com>
2026-04-29 17:14:42 +0200

    Add PalindormeString

---
+++ src/PalindormeString.java
@@ -0,0 +1,18 @@
+public class PalindormeString {
+    public boolean palindrome(String text){
+        for (int i = 0; i < text.length()/2; i++) {
+            if ( text.charAt(i) != text.charAt(text.length() - 1 - i)){
+                return false;
+
+            }
+
+        }
+        return true;
+    }
+
+    static void main(String[] args) {
+        PalindormeString pal = new PalindormeString();
+        System.out.println(pal.palindrome("kajak"));
+        System.out.println(pal.palindrome("krzesło"));
+    }
+}
