48610e6acb18749b9c8e8755135f06e0ef6453bb
Karola <wolnikkarolina1@gmail.com>
2026-04-30 15:45:42 +0200

    Add CountCharMap

---
+++ src/CountCharsMap.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountCharsMap {
+    public Map<Character, Integer> countChars(String text){
+        Map<Character, Integer> count = new HashMap<>();
+        for (int i = 0; i < text.length(); i++) {
+            count.put(text.charAt(i), count.getOrDefault(text.charAt(i),0) + 1);
+        }
+        return count;
+    }
+    // ta linijka zastępuje cały if/else
+    //if ( count.containsKey(text.charAt(i))){
+    //     int value = count.get(text.charAt(i));
+    //     count.replace(text.charAt(i), value + 1);
+    // } else {
+    //     count.put(text.charAt(i), 1);
+    // }
+
+    // porwonaj czy dwa stringi sa anagramami
+    public boolean isAnagram(String text1, String text2){
+        if ( text1.length() != text2.length()){
+            return false;
+        }
+        Map<Character, Integer> map1 = countChars(text1);
+        Map<Character, Integer> map2 = countChars(text2);
+        return map1.equals(map2);
+    }
+
+    static void main(String[] args) {
+        CountCharsMap m = new CountCharsMap();
+        System.out.println(m.countChars("aabbyyciiiii"));
+        Map<Character, Integer> map = m.countChars("ajjjeeennnnnniejeeee");
+        for (Map.Entry<Character, Integer> e : map.entrySet()) {
+            System.out.println(e.getKey() + ";" + e.getValue());
+
+        }
+
+        // porwonaj czy dwa stringi sa anagramami
+        System.out.println(m.isAnagram("listen", "silent"));
+    }
+}
